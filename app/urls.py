fcaf0d3c3fc7364faae43b7ef9ff55438d800d0e
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-05-11 13:02:54 -0300

    Reftoring to use Views with viewsets and urls with DefaultRouter

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
-from django.urls import path
+from app.views import TodoViewSet
 
-urlpatterns = [
-    path('', TodoListAndCreate.as_view()),
-    path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
-]
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
--- app/views.py
+++ app/views.py
@@ -1,22 +1,17 @@
 from app.models import Todo
 from app.serializers import TodoSerializer
 
-from rest_framework import generics
+
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 
-class TodoListAndCreate(generics.ListCreateAPIView):
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
-
-class TodoDetailChangeAndDelete(generics.RetrieveUpdateAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
 
 
 
